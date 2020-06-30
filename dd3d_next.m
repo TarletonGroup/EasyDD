7213e25da2a7caa82feafe0114da9eacf7f6fe6e
Daniel Celis Garza <dcelisgarza@users.noreply.github.com>
2020-06-30 13:18:26 +0100

    Update to latest version.
    * Move vertices from input to `finiteElement3D.m`.
    * Add analytic tractions to dd3d_next.

--- dd3d_next.m
+++ dd3d_next.m
@@ -3,7 +3,7 @@
 % % mixed boundary conditions cantilever.
 % % linear mobility law (mobfcc0,mobfcc1)
 % % N^2 interaction (no neighbor list, no fast multipole)
-% 
+%
 % %Data structure:
 % %NMAX:    maximum number of nodes (including disabled ones)
 % %LINKMAX: maximum number of links (including disabled ones)
@@ -11,7 +11,7 @@
 % %vn: (NMAX,3) array of nodal velocities
 % %fn: (NMAX,3) array of nodal forces
 % %links: (LINKMAX,8) array of links (id1,id2,bx,by,bz,nx,ny,nz)
-% 
+%
 % compile the c source code for seg-seg force evaluation and makes a dynamic linked library
 %  disp('Compiling MEX files');
 %  mex -v COPTIMFLAGS="-O3 -Oy -DNDEBUG" SegSegForcesMex.c
@@ -22,24 +22,61 @@
 %  mex -v COPTIMFLAGS="-O3 -Oy -DNDEBUG" mobbcc1mex.c
 %  mex -v COPTIMFLAGS="-O3 -Oy -DNDEBUG" displacementmex_et.c
 %  mex -v COPTIMFLAGS="-O3 -Oy -DNDEBUG" CreateInputMex.c %CollisionMarielle
-%  mex -v COPTIMFLAGS="-O3 -Oy -DNDEBUG" CollisionCheckerMexMariellebis.c %CollisionMarielle 
+%  mex -v COPTIMFLAGS="-O3 -Oy -DNDEBUG" CollisionCheckerMexMariellebis.c %CollisionMarielle
+%  mex -v COPTIMFLAGS="-O3 -Oy -DNDEBUG" ./nodalForce/nodal_surface_force_linear_rectangle_mex.c
+%  mexcuda -v COMPFLAGS="-Xptxas -v -arch=compute_60 -code=sm_60 -use_fast_math" ./nodalForce/nodal_surface_force_linear_rectangle_mex_cuda.cu
 %   disp('Done!');
 %  default value if run by itself (e.g. not through "rundd3d")
 %  cleanup the empty node and link entries at the end of the initial data structures
 [rn,links]=cleanupnodes(rn,links);
 
 % genererate the connectivity list from the list of links
-disp('Initiliazing connectivity list. Please wait.'); 
+disp('Initiliazing connectivity list. Please wait.');
 [connectivity,linksinconnect]=genconnectivity(rn,links,maxconnections);
 
 consistencycheck(rn,links,connectivity,linksinconnect);
-disp('Consistencycheck : Done!'); 
+disp('Consistencycheck : Done!');
 
 %construct stiffeness matrix K and pre-compute L,U decompositions.
-disp('Constructing stiffness matrix K and precomputing L,U decompositions. Please wait.'); 
-[B,xnodes,mno,nc,n,D,kg,K,L,U,Sleft,Sright,Stop,Sbot,...
+disp('Constructing stiffness matrix K and precomputing L,U decompositions. Please wait.');
+[vertices,B,xnodes,mno,nc,n,D,kg,K,L,U,Sleft,Sright,Stop,Sbot,...
     Sfront,Sback,gammat,gammau,gammaMixed,fixedDofs,freeDofs,...
-    w,h,d,my,mz,mel] = finiteElement3D(dx,dy,dz,mx,MU,NU,loading);    
+    w,h,d,my,mz,mel] = finiteElement3D(dx,dy,dz,mx,MU,NU,loading);
+
+%% Addition by Daniel Celis Garza 12/19/2017.
+% Precomputing variables needed to couple tractions induced by dislocations
+% to FEM. Generate the list of planes to be extracted. The FEM coupler is
+% hardcoded so that the min(x) yz-plane does not have traction boundary
+% conditions.
+if a_trac ~= 0
+    f_hat = zeros(3*mno, 1);
+
+    planes = (1:1:6)';
+    [x3x6_lbl, x3x6, n_se] = extract_surface_nodes(xnodes, nc, [mx;my;mz],...
+                                                   planes, 4);
+    gamma_dln = [gammat(:,1); gammaMixed(:,1)];
+    [f_dln_node, f_dln_se,...
+     f_dln, idxi, n_nodes_t] = nodal_force_map(x3x6_lbl, gamma_dln, 4, n_se, mno);
+    tolerance = dx/10^7;
+    % Parallel CUDA C flags.
+    if use_gpu == 1
+        % Provide a default number of threads in case none is given.
+        if ~exist('n_threads', 'var')
+            n_threads = 256;
+        end %if
+        % Provide a default parallelisaion scheme in case none is given.
+        if ~exist('para_scheme', 'var')
+            % Parallelise over dislocations.
+            para_scheme = 1;
+        end %if
+    else
+        n_threads = 0;
+        para_scheme = 0;
+    end %if
+
+    gamma_disp = [gammau; gammaMixed];
+end
+
 disp('Done! Initializing simulation.');
 
 global USE_GPU;
@@ -48,7 +85,7 @@ USE_GPU=1; %0 if CPU only.
 if (USE_GPU==1)
     disp('Going to use GPU as well...'); % setenv('PATH', [getenv('PATH') ';C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\bin\amd64']);
     system('nvcc -ptx -m64 -arch sm_35 SegForceNBodyCUDADoublePrecision.cu');
-end  
+end
 
 
 %Use Delaunay triangulation to create surface mesh, used for visualisation
@@ -60,8 +97,8 @@ end
 %disp('Remeshing...');
 [rn,links,connectivity,linksinconnect]=remesh_surf(rn,links,connectivity,linksinconnect,vertices,TriangleCentroids,TriangleNormals);
 
-%plot dislocation structure           
-% figure(1); 
+%plot dislocation structure
+% figure(1);
 % plotHandle = plotnodes(rn,links,plim,vertices); view(viewangle);
 % drawnow
 
@@ -87,7 +124,7 @@ utilda_0(3*gn -1) = Uy;
 utilda_0(3*gn   ) = Uz;
 %%
 while simTime < totalSimTime
-    
+
     % frame recording
 %     intSimTime=intSimTime+dt;
 %     if intSimTime > dtplot && doplot == 1
@@ -104,28 +141,35 @@ while simTime < totalSimTime
 %         ylim([-100 100])
 %         intSimTime=intSimTime-dtplot;
 %     end
-    
+
     %DDD+FEM coupling
-    [uhat,fend,Ubar] = FEMcoupler(rn,links,maxconnections,a,MU,NU,xnodes,mno,kg,L,U,...
-                    gammau,gammat,gammaMixed,fixedDofs,freeDofs,dx,dy,dz,simTime,mx,my,mz,utilda_0);
+    if a_trac == 0
+        [uhat,fend,Ubar] = FEMcoupler(rn,links,maxconnections,a,MU,NU,xnodes,mno,kg,L,U,...
+                        gammau,gammat,gammaMixed,fixedDofs,freeDofs,dx,dy,dz,simTime,mx,my,mz,utilda_0);
+    else
+        [uhat,fend,Ubar] = analytic_FEMcoupler(rn,links,a,MU,NU,xnodes,mno,kg,L,U,...
+            gamma_disp, gammat, gamma_mixed, fixedDofs,freeDofs,dx,dy,dz,mx,my,mz,utilda_0,t,...
+            gamma_dln, x3x6, n_nodes, n_nodes_t, n_se, idxi, ...
+            f_dln_node, f_dln_se, f_dln, f_hat, use_gpu, n_threads, para_scheme, para_tol);
+    end
     Fend(curstep+1) = fend;
     U_bar(curstep+1) = Ubar;
     t(curstep+1) = simTime;
-    
+
     fprintf('fend = %d, Ubar = %d, simTime = %d \n',fend,Ubar,simTime);
-    
+
 %     if (dovirtmesh)
 %         %remeshing virtual dislocation structures
 %         %[rn,links,connectivity,linksinconnect]=remesh_surf(rn,links,connectivity,linksinconnect,vertices,TriangleCentroids,TriangleNormals);
 %         %[rn,links,connectivity,linksinconnect] = virtualmeshcoarsen2(rn,links,maxconnections,10*lmin);
 %     end
-    
+
     %integrating equation of motion
     [rnnew,vn,dt,fn,fseg]=feval(integrator,rn,dt,dt0,MU,NU,a,Ec,links,connectivity,...
         rmax,rntol,mobility,vertices,uhat,nc,xnodes,D,mx,mz,w,h,d);
     % plastic strain and plastic spin calculations
     [ep_inc,wp_inc]=calcplasticstrainincrement(rnnew,rn,links,(2*plim)^3);
-    
+
     if(mod(curstep,printfreq)==0) && not(isempty(vn))
         fprintf('step%3d dt=%e v%d=(%e,%e,%e) \n',...
             curstep,dt,printnode,vn(printnode,1),vn(printnode,2),vn(printnode,3));
@@ -135,26 +179,26 @@ while simTime < totalSimTime
         fprintf('step%3d dt=%e v%d=(%e,%e,%e) \n',...
             curstep,dt,printnode);
     end
-    
+
      if(mod(curstep,plotfreq)==0)
         plotnodes(rn,links,plim,vertices);
          view(viewangle);
          %schematicplot2(rn,links,vertices,U_bar,Fend,amag,dx,totalSimTime);
          drawnow
-         pause(0.01);  
-%          
+         pause(0.01);
+%
 %         figure(2);
 %          clf
 %          plot(U_bar*bmag,-Fend*bmag^2*mumag)
-%          pause(0.01);  
+%          pause(0.01);
      end
-    
+
     rnnew=[rnnew(:,1:3) vn rnnew(:,4)];
     linksnew=links;
     connectivitynew=connectivity;
     linksinconnectnew=linksinconnect;
     fsegnew=fseg;
-    
+
     if (doremesh) %do virtual re-meshing first
         %remeshing virtual dislocation structures
         if (dovirtmesh)
@@ -167,30 +211,30 @@ while simTime < totalSimTime
         [rnnew,linksnew,connectivitynew,linksinconnectnew,fsegnew]=remesh_all(rnnew,linksnew,connectivitynew,linksinconnectnew,fsegnew,lmin,lmax,areamin,areamax,MU,NU,a,Ec,mobility,doremesh,dovirtmesh,vertices,...
             uhat,nc,xnodes,D,mx,mz,w,h,d,TriangleCentroids,TriangleNormals);
     end
-    
+
     %save restart.mat
     if (docollision)
         s1_old=0;
         s2_old=0;
         %collision detection and handling
-         
+
               %COLLISION GPU / GPUPLUS  Marielle  + CollisionCheckerMexMarielle
               %it requires a GPU device
               %it requires CollisionCheckerMexMarielle, collisionGPUplus (or collision GPU), mindistcalcGPU1, mindistcalcGPU2,CreateInputMex
               colliding_segments=1;
               while colliding_segments==1
-              [colliding_segments,n1s1,n2s1,n1s2,n2s2,floop,s1,s2,segpair]=CollisionCheckerMexMariellebis3(rnnew(:,1),rnnew(:,2),rnnew(:,3),rnnew(:,end),...
+              [colliding_segments,n1s1,n2s1,n1s2,n2s2,floop,s1,s2,segpair]=CollisionCheckerMexMariellebis(rnnew(:,1),rnnew(:,2),rnnew(:,3),rnnew(:,end),...
                rnnew(:,4),rnnew(:,5),rnnew(:,6),linksnew(:,1),linksnew(:,2),connectivitynew,rann);
-                
-             
+
+
 %                   if colliding_segments == 1 %scan and update dislocation structure.
 %                         reset(gpuDevice)
 %                         [rnnew,linksnew,~,~,fsegnew]=...
 %                         collisionGPUplus(rnnew,linksnew,connectivitynew,linksinconnectnew,...
-%                         fsegnew,rann,MU,NU,a,Ec,mobility,vertices,uhat,nc,xnodes,D,mx,mz,w,h,d,floop,n1s1,n2s1,n1s2,n2s2,s1,s2,segpair); 
+%                         fsegnew,rann,MU,NU,a,Ec,mobility,vertices,uhat,nc,xnodes,D,mx,mz,w,h,d,floop,n1s1,n2s1,n1s2,n2s2,s1,s2,segpair);
 %                   end
-                  
-       
+
+
 %              INTIAL COLLISION
 %              [colliding_segments]=CollisionCheckerMex(rnnew(:,1),rnnew(:,2),rnnew(:,3),rnnew(:,end),...
 %              rnnew(:,4),rnnew(:,5),rnnew(:,6),linksnew(:,1),linksnew(:,2),connectivitynew,rann);
@@ -210,7 +254,7 @@ while simTime < totalSimTime
               [rnnew,linksnew,connectivitynew,linksinconnectnew,fsegnew] = cleanupsegments(rnnew,linksnew,fsegnew);
               end
     end
-    
+
     %find blockading fixed nodes and allow code to deal with them
     for i=1:size(rnnew,1)
         if rnnew(i,end)~=7
@@ -220,14 +264,14 @@ while simTime < totalSimTime
             rnnew(i,end)=0;
         end
     end
- 
+
     if (doseparation) && max(connectivitynew(:,1))>3
         %spliting of nodes with 4 or more connections
         [rnnew,linksnew,connectivitynew,linksinconnectnew,fsegnew]=...
             separation(rnnew,linksnew,connectivitynew,linksinconnectnew,...
             fsegnew,mobility,MU,NU,a,Ec,2*rann,vertices,uhat,nc,xnodes,D,mx,mz,w,h,d);
     end
-    
+
     [rnnew,linksnew,connectivitynew,linksinconnectnew,fsegnew]=remesh_all(rnnew,linksnew,connectivitynew,linksinconnectnew,fsegnew,lmin,lmax,areamin,areamax,MU,NU,a,Ec,mobility,doremesh,0,vertices,...
             uhat,nc,xnodes,D,mx,mz,w,h,d,TriangleCentroids,TriangleNormals);
 
@@ -237,7 +281,7 @@ while simTime < totalSimTime
     connectivity=connectivitynew;
     linksinconnect=linksinconnectnew;
     fseg=fsegnew;
-      
+
     %store run time information
     %time step
     curstep = curstep + 1;
@@ -249,10 +293,10 @@ while simTime < totalSimTime
 %         disp('Dislocation-free real domain. Only virtual segments remain!');
 %         disp('Computing distorted mesh. Please wait...');
 %         [utilda]=visualise(rn,links,NU,D,mx,my,mz,mel,mno,xnodes,nc,...
-%             dx,dy,dz,w,h,d,vertices,uhat);                       
+%             dx,dy,dz,w,h,d,vertices,uhat);
 %         return;
 %     end
-        
+
  end
 
 save restart;
--- finiteElement3D.m
+++ finiteElement3D.m
@@ -1,16 +1,16 @@
 % clear all
-% mx=1,dx=6,dy=1,dz=1,mu=1,nu=.3,loading=1; 
+% mx=1,dx=6,dy=1,dz=1,mu=1,nu=.3,loading=1;
 
-function [B,xnodes,mno,nc,n,D,kg,K,L,U,Sleft,Sright,Stop,Sbot,...
+function [vertices,B,xnodes,mno,nc,n,D,kg,K,L,U,Sleft,Sright,Stop,Sbot,...
     Sfront,Sback,gammat,gammau,gammaMixed,fixedDofs,freeDofs,...
-    w,h,d,my,mz,mel,unfixedDofs] = finiteElement3D(dx,dy,dz,mx,mu,nu,loading)         
+    w,h,d,my,mz,mel,unfixedDofs] = finiteElement3D(dx,dy,dz,mx,mu,nu,loading)
 
 % E Tarleton edmund.tarleton@materials.ox.ac.uk
 % 3D FEM code using linear 8 node element with 8 integration pts (2x2x2) per element
 % 4. ----- .3
 %  �\       �\
 %  � \      � \
-% 1. -\---- .2 \ 
+% 1. -\---- .2 \
 %   \  \     \  \
 %    \ 8. ----\- .7
 %     \ �      \ �
@@ -22,11 +22,20 @@ function [B,xnodes,mno,nc,n,D,kg,K,L,U,Sleft,Sright,Stop,Sbot,...
 % (y going in to the page) note this is rotated about x axis w.r.t. local (s1,s2,s3)
 % system
 % -------------------------------------------------
-%           
+%
 % ^z                   (mx,my)
-% |          
-% |           
+% |
+% |
 % |------>x-----------------------------------------
+% Simulation domain vertices. Required for surface remeshing.
+vertices = [0,0,0;...
+            dx,0,0;...
+            0,dy,0;...
+            dx,dy,0;...
+            0,0,dz;...
+            dx,0,dz;...
+            0,dy,dz;...
+            dx,dy,dz];
 % not that this code uses a constant element size in base and beam: (w,h,d)
 % also gammau is the left surface (eg baseLength=0)
 loading =1;
@@ -44,7 +53,7 @@ d = dz/mz; % element depth
 mel = mx*my*mz;
 mno=(mx+1)*(my+1)*(mz+1);
 
-    
+
 ey =mu*(2*(1+nu));
 la =nu*ey/((1+nu)*(1-2*nu));
 
@@ -65,18 +74,18 @@ D(2,3) = la;
 % D(1,2) = ey*nu/(1-nu^2);
 % D(2,2) = D(1,1);
 % D(3,3) = ey/2/(1+nu);
-% 
+%
 D(2,1) = D(1,2);
 D(3,1) = D(1,3);
 D(3,2) = D(2,3);
 
 
-%           
+%
 %  ^s2
-%  |          
-%  |           
+%  |
+%  |
 %  |------>s1
-% 
+%
 %(s3 out of page)
 zv = 1/sqrt(3);
 z = zeros(8,3);
@@ -92,18 +101,18 @@ z(8,1:3) = [-zv, zv, zv];
 xnodes= zeros(mno,3); %global nodal coordinates gn
 
 %back face y = dy, C = (mx+1)*(mz+1)*my
-%1+mz*(mx+1)+C ...          (mx+1)*(mz+1)+C 
+%1+mz*(mx+1)+C ...          (mx+1)*(mz+1)+C
 % .          .                  .
 % .            .                .
 % 1+(mx+1)+C  2+(mx+1)+C  ...   2*(mx+1)+C
 % 1+C         2+C         ...     mx+1 + C
 
 %    front face y = 0
-%    1+mz*(mx+1) ...          (mx+1)*(mz+1) 
+%    1+mz*(mx+1) ...          (mx+1)*(mz+1)
 %    .          .                  .
 %    .            .                .
 %    1+(mx+1)  2+(mx+1)  ...   2*(mx+1)
-%    1         2         ...     mx+1 
+%    1         2         ...     mx+1
 
 for k = 1:mz+1
     for j =1:my+1 % nodes in x direction
@@ -116,12 +125,12 @@ for k = 1:mz+1
     end
 
 end
-   
+
 nc = zeros(mel,4); %element connectivity
 % 4. ----- .3
 %  �\       �\
 %  � \      � \
-% 1. -\---- .2 \ 
+% 1. -\---- .2 \
 %   \  \     \  \
 %    \ 8. ----\- .7
 %     \ �      \ �
@@ -137,7 +146,7 @@ for k =1:mz
             nc(ge,1) = i + (k-1)*(mx+1) + (mx+1)*(mz+1)*j;
             nc(ge,2) = nc(ge,1)+1;
             nc(ge,4) = i +  k*(mx+1) + (mx+1)*(mz+1)*j;
-            nc(ge,3) = nc(ge,4) + 1;            
+            nc(ge,3) = nc(ge,4) + 1;
             nc(ge,5) = i + (k-1)*(mx+1) + (mx+1)*(mz+1)*(j-1);
             nc(ge,6) = nc(ge,5)+1;
             nc(ge,8) = i + k*(mx+1) + (mx+1)*(mz+1)*(j-1);
@@ -151,17 +160,17 @@ end
 % for p =1:mel
 % %     plot3(x(:,1),x(:,2),x(:,3),'.') % plot nodes
 %     % plot elements
-%     plot3(xnodes(nc(p,[1:4,1]),1),xnodes(nc(p,[1:4,1]),2),xnodes(nc(p,[1:4,1]),3),'-') 
-%     plot3(xnodes(nc(p,[5:8,5]),1),xnodes(nc(p,[5:8,5]),2),xnodes(nc(p,[5:8,5]),3),'-') 
-%     plot3(xnodes(nc(p,[1,5]),1),xnodes(nc(p,[1,5]),2),xnodes(nc(p,[1,5]),3),'-') % 
-%     plot3(xnodes(nc(p,[2,6]),1),xnodes(nc(p,[2,6]),2),xnodes(nc(p,[2,6]),3),'-') % 
-%     plot3(xnodes(nc(p,[3,7]),1),xnodes(nc(p,[3,7]),2),xnodes(nc(p,[3,7]),3),'-') % 
-%     plot3(xnodes(nc(p,[4,8]),1),xnodes(nc(p,[4,8]),2),xnodes(nc(p,[4,8]),3),'-') % 
+%     plot3(xnodes(nc(p,[1:4,1]),1),xnodes(nc(p,[1:4,1]),2),xnodes(nc(p,[1:4,1]),3),'-')
+%     plot3(xnodes(nc(p,[5:8,5]),1),xnodes(nc(p,[5:8,5]),2),xnodes(nc(p,[5:8,5]),3),'-')
+%     plot3(xnodes(nc(p,[1,5]),1),xnodes(nc(p,[1,5]),2),xnodes(nc(p,[1,5]),3),'-') %
+%     plot3(xnodes(nc(p,[2,6]),1),xnodes(nc(p,[2,6]),2),xnodes(nc(p,[2,6]),3),'-') %
+%     plot3(xnodes(nc(p,[3,7]),1),xnodes(nc(p,[3,7]),2),xnodes(nc(p,[3,7]),3),'-') %
+%     plot3(xnodes(nc(p,[4,8]),1),xnodes(nc(p,[4,8]),2),xnodes(nc(p,[4,8]),3),'-') %
 % end
 % axis('equal')
 % hold off
 
-n = zeros(8,8); 
+n = zeros(8,8);
 % n(q,a) is shape function Na(s1,s2,s3) evaluated at integration point q
 for q =1:8
     n(q,1) = 1/8*(1-z(q,1))*(1-z(q,2))*(1-z(q,3));
@@ -175,8 +184,8 @@ for q =1:8
 end
 
 
-ns = zeros(8,8,3); 
-% derivative of shape function Na(s1,s2,s3)  w.r.t. sj evaluated at 
+ns = zeros(8,8,3);
+% derivative of shape function Na(s1,s2,s3)  w.r.t. sj evaluated at
 %integration point q  ns(q,a,j) = (dNa/dsj)s=zq
 for q =1:8
     ns(q,1,1) = -1/8*(1-z(q,2))*(1-z(q,3));
@@ -191,7 +200,7 @@ for q =1:8
     ns(q,4,1) = -1/8*(1+z(q,2))*(1-z(q,3));
     ns(q,4,2) = 1/8*(1-z(q,1))*(1-z(q,3));
     ns(q,4,3) = -1/8*(1-z(q,1))*(1+z(q,2));
-    
+
     ns(q,5,1) = -1/8*(1-z(q,2))*(1+z(q,3));
     ns(q,5,2) = -1/8*(1-z(q,1))*(1+z(q,3));
     ns(q,5,3) = 1/8*(1-z(q,1))*(1-z(q,2));
@@ -213,7 +222,7 @@ pm3 =[-1 -1 -1 -1  1  1  1  1];
 % pm1 =[-1  1  1 -1 -1  1  1 -1]
 % pm2 =[-1 -1  1  1 -1 -1  1  1]
 % pm3 =[-1 -1 -1 -1  1  1  1  1]
-dNds=zeros(8,8,3); 
+dNds=zeros(8,8,3);
 % Na(s1,s2,s3) = 1/8 *(1+pm1(a)s1)(1+pm2(a)s2)(1+pm3(a)s3)
 for q=1:8
     for a=1:8
@@ -222,7 +231,7 @@ for q=1:8
         dNds(q,a,3) = 1/8*(1+pm1(a)*z(q,1))*(1+pm2(a)*z(q,2))*pm3(a);
     end
 end
-if any(dNds~=ns) 
+if any(dNds~=ns)
     'error'
     pause
 end
@@ -235,59 +244,59 @@ B = zeros(6,24,mel,8); % (# strain components, # dof/element, # elements, int pt
 
 for p = 1:mel % all elements
     for q = 1:8 % integration points per element
-        
+
         for i =1:3 % DOF
             for j = 1:3 % DOF
-                 J(i,j,p,q) = ns(q,1:8,j)*xnodes(nc(p,1:8),i); 
-                 % implied sum over a: local shape function number                
-                % sum over a of dNa(s1,s2,s3)/dsj at S=z(q) *xi(local node a of element p) 
-                %Jij= dxi/dsj evaluated at element p integration point q                
+                 J(i,j,p,q) = ns(q,1:8,j)*xnodes(nc(p,1:8),i);
+                 % implied sum over a: local shape function number
+                % sum over a of dNa(s1,s2,s3)/dsj at S=z(q) *xi(local node a of element p)
+                %Jij= dxi/dsj evaluated at element p integration point q
             end
-        end                            
-        
+        end
+
         detJ(p,q) = det(J(:,:,p,q)); % det(J) evaluated at element p int point q
 
         invJ = inv(J(:,:,p,q)); % invJ_ij = dsi/dxj
-        
+
         for a =1:8
             for j =1:3
                 for i =1:3
                     nx(p,q,a,j) = nx(p,q,a,j)+ns(q,a,i)*invJ(i,j);
                 end
             end
-            
+
             B(1,(a-1)*3+1,p,q)=nx(p,q,a,1);
             B(1,(a-1)*3+2,p,q)=0;
             B(1,(a-1)*3+3,p,q)=0;
-            
+
             B(2,(a-1)*3+1,p,q)=0;
             B(2,(a-1)*3+2,p,q)=nx(p,q,a,2);
             B(2,(a-1)*3+3,p,q)=0;
-            
+
             B(3,(a-1)*3+1,p,q)=0;
             B(3,(a-1)*3+2,p,q)=0;
             B(3,(a-1)*3+3,p,q)=nx(p,q,a,3);
-            
+
             B(4,(a-1)*3+1,p,q)=nx(p,q,a,2);
             B(4,(a-1)*3+2,p,q)=nx(p,q,a,1);
             B(4,(a-1)*3+3,p,q)=0;
-            
+
             B(5,(a-1)*3+1,p,q)=nx(p,q,a,3);
             B(5,(a-1)*3+2,p,q)=0;
             B(5,(a-1)*3+3,p,q)=nx(p,q,a,1);
-            
+
             B(6,(a-1)*3+1,p,q)=0;
             B(6,(a-1)*3+2,p,q)=nx(p,q,a,3);
             B(6,(a-1)*3+3,p,q)=nx(p,q,a,2);
-            
+
         end
     end
 end
 
 
-disp('local K...');        
+disp('local K...');
 ke = zeros(24,24,mel); %local stiffness matrix
-for p =1:mel %all elements 
+for p =1:mel %all elements
     for q = 1:8 % int points per element
         ke(:,:,p) = ke(:,:,p)+B(:,:,p,q)'*D*B(:,:,p,q)*detJ(p,q);
     end
@@ -306,7 +315,7 @@ disp('global K...');
 %     gna=nc(p,a);
 %     gnb=nc(p,b);
 %     for i =1:3
-%         for j=1:3        
+%         for j=1:3
 %             kg(3*(gna-1)+i,3*(gnb-1)+j)= kg(3*(gna-1)+i,3*(gnb-1)+j)+ke(3*(a-1)+i,3*(b-1)+j,p);
 %         end
 %     end
@@ -330,12 +339,12 @@ disp('global K...');
 % %     dofLocal=[3*(a-1)+1,3*(a-1)+2,3*(a-1)+3];
 % % %     dofbl=[3*(b-1)+1,3*(b-1)+2,3*(b-1)+3]
 % %     kg(dof,dof)= kg(dof,dof)+ke(dofLocal,dofLocal,p);
-% %     
+% %
 % % end
 
 %% kg Haiyang
 tic
-a=1:8; %local node numbers 
+a=1:8; %local node numbers
 dofLocal=[3*(a-1)+1,3*(a-1)+2,3*(a-1)+3];
 ntriplets = 3*mno;
 I = zeros(ntriplets,1);
@@ -371,91 +380,91 @@ bcwt=mean(diag(kg));%=trace(K)/length(K)
 bcwt = full(bcwt);
 
 if loading == 1||loading==0 % cantilever bending
-    % define empty sets (not used but allows same code as for bending)       
+    % define empty sets (not used but allows same code as for bending)
     % priority for inclusion of shared nodes is in order below
     %S = [node #, nodal Area, outward normal]
     Smixed = zeros(my+1,5); % top right edge (u3=U) first priority
-    
+
     Sleft = zeros((my+1)*(mz+1),5);  % second priority
-    Sright = zeros((my+1)*mz,3); 
-    
+    Sright = zeros((my+1)*mz,3);
+
     Stop =  zeros((mx-1)*(my+1),5);  % third priority
-    Sbot =  zeros((mx-1)*(my+1),5);  
+    Sbot =  zeros((mx-1)*(my+1),5);
 
     Sfront = zeros((mz-1)*(mx-1),5); % fourth priority
     Sback = zeros((mz-1)*(mx-1),5);
-    
-   
+
+
     m=1;
     for j =1:my+1
-        for k =1:mz+1 
+        for k =1:mz+1
             % left surface of beam (u=0)
             gn = 1+(k-1)*(mx+1)+(mx+1)*(mz+1)*(j-1);
-            Sleft(m,1)= gn; 
-            
+            Sleft(m,1)= gn;
+
             if k == 1 || k == mz+1
-                L3 = 0.5*d;            
+                L3 = 0.5*d;
             else
                 L3 = d;
             end
-            
+
             if j ==1 || j == my+1
                 L2 = 0.5*h;
             else
                 L2 = h;
-            end 
-            
+            end
+
             Sleft(m,2) = L2*L3;
             Sleft(m,3:5) = [-1,0,0];
             m=m+1;
         end
     end
-    
+
     m=1;
     for j =1:my+1
         for k =1:mz
             % right surface of beam (t=0)
-            gn = k*(mx+1)+(mx+1)*(mz+1)*(j-1);                 
+            gn = k*(mx+1)+(mx+1)*(mz+1)*(j-1);
             Sright(m,1)= gn;
-                        
+
             if k == 1
-                L3 = 0.5*d;            
+                L3 = 0.5*d;
             else
                 L3 = d;
             end
-            
+
             if j ==1 || j == my+1
                 L2 = 0.5*h;
             else
                 L2 = h;
-            end 
-            
+            end
+
             Sright(m,2) = L2*L3;
             Sright(m,3:5) = [1,0,0];
             m=m+1;
-        end               
+        end
     end
-    
+
     m=1;
     for j =1:my+1
         % top right loaded edge (t1=0, u2=-U) mixed surface
         gn = (mx+1)*(mz+1)*j;
         Smixed(m,1) = gn;
-        
+
         if j ==1 || j == my+1
             L2 = 0.5*h;
         else
             L2 = h;
         end
-        
+
         L3 = 0.5*d;
         Smixed(m,2) = L2*L3;
         Smixed(m,3:5) = [1,0,0];
         m=m+1;
     end
-      
-    
-    m=1; 
+
+
+    m=1;
     for j = 1:my+1
         for i=2:mx
             % bottom surface of beam (t=0)
@@ -464,103 +473,103 @@ if loading == 1||loading==0 % cantilever bending
             % top surface of beam (t=0)
             gn = i+(mx+1)*mz+(mx+1)*(mz+1)*(j-1);
             Stop(m,1) = gn;
-            
+
             L1 = w;
             if j == 1 || j == my+1
                 L2 = 0.5*h;
             else
                 L2 = h;
             end
-            
+
             Sbot(m,2) = L1*L2;
             Sbot(m,3:5) = [0,0,-1];
             Stop(m,2) = L1*L2;
-            Stop(m,3:5) = [0,0,1];            
-            m=m+1;            
+            Stop(m,3:5) = [0,0,1];
+            m=m+1;
         end
     end
-    
+
     m=1;
     for k=2:mz
         for i = 2:mx
-            %front surface of beam (t=0)  
+            %front surface of beam (t=0)
             gn = i + (k-1)*(mx+1);
             Sfront(m) = gn; %(y=0)
-            
+
             gn =  i + (k-1)*(mx+1) + (mx+1)*(mz+1)*my;
             Sback(m) = gn; %(y=dy)
-            
+
             L1 = w;
             L3 = d;
             Sfront(m,2) = L1*L3;
             Sfront(m,3:5) = [0,-1,0];
             Sback(m,2) = L1*L3;
             Sback(m,3:5) = [0,1,0];
-            
+
             m=m+1;
         end
     end
-    
+
 else
-    disp('error; only coded for end loadeded cantilever')   
+    disp('error; only coded for end loadeded cantilever')
     pause
 end
 
 if loading ==1 % end loaded cantilever
-           
-    gammat=[Stop;Sbot;Sright;Sfront;Sback]; % t=0         
-    gammau=Sleft; %  set containg fixed nodes u=0    
+
+    gammat=[Stop;Sbot;Sright;Sfront;Sback]; % t=0
+    gammau=Sleft; %  set containg fixed nodes u=0
     gammaMixed=Smixed; % t1=t2=0, u3= U
-    
+
     fixedDofs =[3*gammau(:,1)-2; 3*gammau(:,1)-1; 3*gammau(:,1); 3*gammaMixed(:,1)];
     freeDofs = [3*gammat(:,1)-2; 3*gammat(:,1)-1; 3*gammat(:,1); 3*gammaMixed(:,1)-2;...
         3*gammaMixed(:,1)-1];
-   
+
 elseif loading == 0 % used for debugging
-    
-     Sright = [Sright;Smixed]; 
-%     
-%    
-%     gnr = (mx+1);                   % x(gnr) = [dx 0 0] u3 = 0 -> R(x2)=0; u2 = 0 -> R(x3)=0 
-%     gnl = [(mx+1)*(mz+1)*my+1];     % x(gnl) = [0 dy 0] u3 = 0 -> R(x1)=0                                 
-%     
+
+     Sright = [Sright;Smixed];
+%
+%
+%     gnr = (mx+1);                   % x(gnr) = [dx 0 0] u3 = 0 -> R(x2)=0; u2 = 0 -> R(x3)=0
+%     gnl = [(mx+1)*(mz+1)*my+1];     % x(gnl) = [0 dy 0] u3 = 0 -> R(x1)=0
+%
 %     Smixed = [Sright(ismember(Sright(:,1), gnr),:); Sleft(ismember(Sleft(:,1),gnl),:)];
-%      
-%     
+%
+%
 %     gammat=[Stop;Sbot;Sfront;Sback;...
-%         Sright(~ismember(Sright(:,1), gnr),:); Sleft(~ismember(Sleft(:,1),[1,gnl]),:)]; % t=0            
-%    
-%     gammau = Sleft(ismember(Sleft(:,1),1),:); 
-%     
+%         Sright(~ismember(Sright(:,1), gnr),:); Sleft(~ismember(Sleft(:,1),[1,gnl]),:)]; % t=0
+%
+%     gammau = Sleft(ismember(Sleft(:,1),1),:);
+%
 %     gammaMixed=Smixed;
 % fixedDofs =[3*gammau(:,1)-2; 3*gammau(:,1)-1; 3*gammau(:,1);...
-%         3*gammaMixed(1,1);  3*gammaMixed(1,1)-1; 3*gammaMixed(2,1)];    
+%         3*gammaMixed(1,1);  3*gammaMixed(1,1)-1; 3*gammaMixed(2,1)];
 %   freeDofs = [3*gammat(:,1)-2; 3*gammat(:,1)-1; 3*gammat(:,1);...
 %         3*gammaMixed(1,1)-2; 3*gammaMixed(2,1)-2; 3*gammaMixed(2,1)-1];
 
     Smixed = [];
     gammaMixed=Smixed;
     gammat = [];
-    gammau = [Stop;Sbot;Sleft;Sright;Sfront;Sback]; 
-    
-    fixedDofs =[3*gammau(:,1)-2; 3*gammau(:,1)-1; 3*gammau(:,1)];        
-    
+    gammau = [Stop;Sbot;Sleft;Sright;Sfront;Sback];
+
+    fixedDofs =[3*gammau(:,1)-2; 3*gammau(:,1)-1; 3*gammau(:,1)];
+
     freeDofs = [];
 else
    disp('loading not specified correctly')
    pause
 end
 
-for m = 1:length(fixedDofs)    
+for m = 1:length(fixedDofs)
     i = fixedDofs(m);
     K(:,i) = 0;
     K(i,:) = 0;
-    K(i,i) = bcwt; 
+    K(i,i) = bcwt;
 end
 
  if length([fixedDofs;freeDofs])>length(unique([fixedDofs;freeDofs]))
         disp('error')
-        pause    
+        pause
  end
 
 % {freeDofs,fixedDofs} should contain every degree of freedom on boundary +
@@ -580,7 +589,7 @@ hold off
 % disp('LU decomposition of K...')
 % %L=0;U=0;
 % tic;
-% [L,U] = lu(K);    
+% [L,U] = lu(K);
 % toc;
 
 
