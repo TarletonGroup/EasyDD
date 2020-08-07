f6a37a061e4c1734d57460f3f7057531f8d36b2f
Daniel Celis Garza <dcelisgarza@users.noreply.github.com>
2020-08-07 11:56:17 +0100

    Create code graveyard and move old main file into it.

--- code_graveyard/code_graveyard.m
+++ code_graveyard/code_graveyard.m
@@ -1,3 +1,14 @@
+%%=======================================================================%%
+% This file unused functions and versions of functions. We keep them in
+% case bits and pieces prove useful in the future. However, anything in
+% this file should be treated as a repository of deprecated and outdated
+% functionality whose only use is for reference.
+%%=======================================================================%%
+
+function EasyDD()
+%%=======================================================================%%
+% One of the many old versions of EasyDD.
+%%=======================================================================%%
 % %Dislocation Dynamics simulation in 3-dimension
 % % Features:
 % % mixed boundary conditions cantilever.
@@ -56,10 +67,10 @@ f_hat = zeros(3*mno, 1);
 
 planes = (1:1:6)';
 [x3x6_lbl, x3x6, n_se] = extract_surface_nodes(xnodes, nc, [mx;my;mz],...
-                                               planes, 4);
+    planes, 4);
 gamma_dln = [gammat(:,1); gammaMixed(:,1)];
 [f_dln_node, f_dln_se,...
- f_dln, idxi, n_nodes_t] = nodal_force_map(x3x6_lbl, gamma_dln, 4, n_se, mno);
+    f_dln, idxi, n_nodes_t] = nodal_force_map(x3x6_lbl, gamma_dln, 4, n_se, mno);
 para_tol = dx/1e7;
 % Parallel CUDA C flags.
 if use_gpu == 1
@@ -126,7 +137,7 @@ utilda_0(3*gn -1) = Uy;
 utilda_0(3*gn   ) = Uz;
 %%
 while simTime < totalSimTime
-
+    
     % frame recording
     intSimTime=intSimTime+dt;
     if intSimTime > dtplot && doplot == 1
@@ -142,11 +153,11 @@ while simTime < totalSimTime
         %ylim([-100 100])
         intSimTime=intSimTime-dtplot;
     end
-
+    
     %DDD+FEM coupling
     if a_trac == 0
         [uhat,fend,Ubar] = FEMcoupler(rn,links,maxconnections,a,MU,NU,xnodes,mno,kg,L,U,...
-                        gammau,gammat,gammaMixed,fixedDofs,freeDofs,dx,dy,dz,simTime,mx,my,mz,utilda_0);
+            gammau,gammat,gammaMixed,fixedDofs,freeDofs,dx,dy,dz,simTime,mx,my,mz,utilda_0);
     else
         [uhat,fend,Ubar] = analytic_FEMcoupler(rn,links,a,MU,NU,xnodes,mno,kg,L,U,...
             gamma_disp, gammat, gammaMixed, fixedDofs,freeDofs,dx,dy,dz,simTime,mx,my,mz,utilda_0,...
@@ -157,26 +168,26 @@ while simTime < totalSimTime
     U_bar(curstep+1) = Ubar;
     t(curstep+1) = simTime;
     fprintf('fend = %d, Ubar = %d, simTime = %d \n',fend,Ubar,simTime);
-
-%     if (dovirtmesh)
-%         %remeshing virtual dislocation structures
-%         %[rn,links,connectivity,linksinconnect]=remesh_surf(rn,links,connectivity,linksinconnect,vertices,TriangleCentroids,TriangleNormals);
-%         %[rn,links,connectivity,linksinconnect] = virtualmeshcoarsen2(rn,links,maxconnections,10*lmin);
-%     end
-
-
+    
+    %     if (dovirtmesh)
+    %         %remeshing virtual dislocation structures
+    %         %[rn,links,connectivity,linksinconnect]=remesh_surf(rn,links,connectivity,linksinconnect,vertices,TriangleCentroids,TriangleNormals);
+    %         %[rn,links,connectivity,linksinconnect] = virtualmeshcoarsen2(rn,links,maxconnections,10*lmin);
+    %     end
+    
+    
     %integrating equation of motion
     [rnnew,vn,dt,fn,fseg]=feval(integrator,rn,dt,dt0,MU,NU,a,Ec,links,connectivity,...
         rmax,rntol,mobility,vertices,uhat,nc,xnodes,D,mx,mz,w,h,d);
-
+    
     % Haiyang's screw dislocation correction.
     [rn,links] = cross_slip_BCCrotate5(fseg,rn,links,connectivity,0,...
-                                            0,curstep,0,NU,a,Ec,0,...
-                                            uhat,nc,xnodes,D,mx,mz,w,h,d);
-
+        0,curstep,0,NU,a,Ec,0,...
+        uhat,nc,xnodes,D,mx,mz,w,h,d);
+    
     % plastic strain and plastic spin calculations
     [ep_inc,wp_inc]=calcplasticstrainincrement(rnnew,rn,links,(2*plim)^3);
-
+    
     if(mod(curstep,printfreq)==0) && not(isempty(vn))
         fprintf('step%3d dt=%e v%d=(%e,%e,%e) \n',...
             curstep,dt,printnode,vn(printnode,1),vn(printnode,2),vn(printnode,3));
@@ -186,67 +197,67 @@ while simTime < totalSimTime
         fprintf('step%3d dt=%e v%d=(%e,%e,%e) \n',...
             curstep,dt,printnode);
     end
-
-     if(mod(curstep,plotfreq)==0)
+    
+    if(mod(curstep,plotfreq)==0)
         plotnodes(rn,links,plim,vertices);
-         view(viewangle);
-         drawnow
-         pause(0.01);
-%
-%         figure(2);
-%          clf
-%          plot(U_bar*bmag,-Fend*bmag^2*mumag)
-%          pause(0.01);
-     end
-
+        view(viewangle);
+        drawnow
+        pause(0.01);
+        %
+        %         figure(2);
+        %          clf
+        %          plot(U_bar*bmag,-Fend*bmag^2*mumag)
+        %          pause(0.01);
+    end
+    
     rnnew=[rnnew(:,1:3) vn rnnew(:,4)];
     linksnew=links;
     connectivitynew=connectivity;
     linksinconnectnew=linksinconnect;
     fsegnew=fseg;
-
+    
     %save restart.mat
     if (docollision)
         s1_old=0;
         s2_old=0;
         %collision detection and handling
-
-              %COLLISION GPU / GPUPLUS  Marielle  + CollisionCheckerMexMarielle
-              %it requires a GPU device
-              %it requires CollisionCheckerMexMarielle, collisionGPUplus (or collision GPU), mindistcalcGPU1, mindistcalcGPU2,CreateInputMex
-              colliding_segments=1;
-              while colliding_segments==1
-              [colliding_segments,n1s1,n2s1,n1s2,n2s2,floop,s1,s2,segpair]=CollisionCheckerMexMariellebis(rnnew(:,1),rnnew(:,2),rnnew(:,3),rnnew(:,end),...
-               rnnew(:,4),rnnew(:,5),rnnew(:,6),linksnew(:,1),linksnew(:,2),connectivitynew,rann);
+        
+        %COLLISION GPU / GPUPLUS  Marielle  + CollisionCheckerMexMarielle
+        %it requires a GPU device
+        %it requires CollisionCheckerMexMarielle, collisionGPUplus (or collision GPU), mindistcalcGPU1, mindistcalcGPU2,CreateInputMex
+        colliding_segments=1;
+        while colliding_segments==1
+            [colliding_segments,n1s1,n2s1,n1s2,n2s2,floop,s1,s2,segpair]=CollisionCheckerMexMariellebis(rnnew(:,1),rnnew(:,2),rnnew(:,3),rnnew(:,end),...
+                rnnew(:,4),rnnew(:,5),rnnew(:,6),linksnew(:,1),linksnew(:,2),connectivitynew,rann);
+            if floop==2
+                colliding_segments=0;
+            end
+            
+            %                   if colliding_segments == 1 %scan and update dislocation structure.
+            %                         reset(gpuDevice)
+            %                         [rnnew,linksnew,~,~,fsegnew]=...
+            %                         collisionGPUplus(rnnew,linksnew,connectivitynew,linksinconnectnew,...
+            %                         fsegnew,rann,MU,NU,a,Ec,mobility,vertices,uhat,nc,xnodes,D,mx,mz,w,h,d,floop,n1s1,n2s1,n1s2,n2s2,s1,s2,segpair);
+            %                   end
+            
+            %              INTIAL COLLISION
+            %              [colliding_segments]=CollisionCheckerMex(rnnew(:,1),rnnew(:,2),rnnew(:,3),rnnew(:,end),...
+            %              rnnew(:,4),rnnew(:,5),rnnew(:,6),linksnew(:,1),linksnew(:,2),connectivitynew,rann);
+            if colliding_segments == 1 %scan and update dislocation structure.
+                
+                %                 [rnnew,linksnew,~,~,fsegnew]=...
+                %                 collision(rnnew,linksnew,connectivitynew,linksinconnectnew,...
+                %                 fsegnew,rann,MU,NU,a,Ec,mobility,vertices,uhat,nc,xnodes,D,mx,mz,w,h,d);
+                
                 if floop==2
+                    %                     if s1==s1_old && s2==s2_old
                     colliding_segments=0;
+                    %                     else
+                    %                         s1_old=s1;
+                    %                         s2_old=s2;
+                    %                     end
                 end
-
-%                   if colliding_segments == 1 %scan and update dislocation structure.
-%                         reset(gpuDevice)
-%                         [rnnew,linksnew,~,~,fsegnew]=...
-%                         collisionGPUplus(rnnew,linksnew,connectivitynew,linksinconnectnew,...
-%                         fsegnew,rann,MU,NU,a,Ec,mobility,vertices,uhat,nc,xnodes,D,mx,mz,w,h,d,floop,n1s1,n2s1,n1s2,n2s2,s1,s2,segpair);
-%                   end
-
-%              INTIAL COLLISION
-%              [colliding_segments]=CollisionCheckerMex(rnnew(:,1),rnnew(:,2),rnnew(:,3),rnnew(:,end),...
-%              rnnew(:,4),rnnew(:,5),rnnew(:,6),linksnew(:,1),linksnew(:,2),connectivitynew,rann);
-             if colliding_segments == 1 %scan and update dislocation structure.
-
-%                 [rnnew,linksnew,~,~,fsegnew]=...
-%                 collision(rnnew,linksnew,connectivitynew,linksinconnectnew,...
-%                 fsegnew,rann,MU,NU,a,Ec,mobility,vertices,uhat,nc,xnodes,D,mx,mz,w,h,d);
-
-                if floop==2
-%                     if s1==s1_old && s2==s2_old
-                        colliding_segments=0;
-%                     else
-%                         s1_old=s1;
-%                         s2_old=s2;
-%                     end
-                end
-
+                
                 if mod(curstep,100)~=0 && floop==1
                     nodeids1=connectivitynew(n1s1,1);
                     nodeids1=1:nodeids1;
@@ -277,19 +288,19 @@ while simTime < totalSimTime
                         colliding_segments=0;
                     end
                 end
-
-
+                
+                
                 fprintf(' Segment %d and segment %d are colliding\n',s1,s2);
                 if colliding_segments==1
                     [rnnew,linksnew,~,~,fsegnew,colliding_segments]=collision_basic(rnnew,linksnew,connectivitynew,linksinconnectnew,fsegnew,rann,MU,NU,a,Ec,mobility,vertices,...
                         uhat,nc,xnodes,D,mx,mz,w,h,d,floop,n1s1,n2s1,n1s2,n2s2,s1,s2,segpair);
                 end
-             end
-              %removing links with effective zero Burgers vectors
-              [rnnew,linksnew,connectivitynew,linksinconnectnew,fsegnew] = cleanupsegments(rnnew,linksnew,fsegnew);
-              end
+            end
+            %removing links with effective zero Burgers vectors
+            [rnnew,linksnew,connectivitynew,linksinconnectnew,fsegnew] = cleanupsegments(rnnew,linksnew,fsegnew);
+        end
     end
-
+    
     %find blockading fixed nodes and allow code to deal with them
     for i=1:size(rnnew,1)
         if rnnew(i,end)~=7
@@ -299,73 +310,71 @@ while simTime < totalSimTime
             rnnew(i,end)=0;
         end
     end
-
+    
     if (doseparation) && max(connectivitynew(:,1))>3
         %spliting of nodes with 4 or more connections
         [rnnew,linksnew,connectivitynew,linksinconnectnew,fsegnew]=...
             separation(rnnew,linksnew,connectivitynew,linksinconnectnew,...
             fsegnew,mobility,MU,NU,a,Ec,2*rann,vertices,uhat,nc,xnodes,D,mx,mz,w,h,d);
     end
-
+    
     if (doremesh) %do virtual re-meshing first
         %remeshing virtual dislocation structures
         if (dovirtmesh)
-           %[rnnew,linksnew,connectivitynew,linksinconnectnew,fsegnew]=virtualmeshcoarsen_mex(rnnew,linksnew,connectivitynew,linksinconnectnew,fsegnew,DIST_SOURCE*0.49,dx,MU,NU,a,Ec);
-           [rnnew,linksnew,connectivitynew,linksinconnectnew,fsegnew]=virtualmeshcoarsen(rnnew,linksnew,connectivitynew,linksinconnectnew,fsegnew,DIST_SOURCE*0.49,dx,MU,NU,a,Ec);
-           %[rnnew,linksnew,connectivitynew,linksinconnectnew] = virtualmeshcoarsen2(rnnew,linksnew,maxconnections,10*lmin);
+            %[rnnew,linksnew,connectivitynew,linksinconnectnew,fsegnew]=virtualmeshcoarsen_mex(rnnew,linksnew,connectivitynew,linksinconnectnew,fsegnew,DIST_SOURCE*0.49,dx,MU,NU,a,Ec);
+            [rnnew,linksnew,connectivitynew,linksinconnectnew,fsegnew]=virtualmeshcoarsen(rnnew,linksnew,connectivitynew,linksinconnectnew,fsegnew,DIST_SOURCE*0.49,dx,MU,NU,a,Ec);
+            %[rnnew,linksnew,connectivitynew,linksinconnectnew] = virtualmeshcoarsen2(rnnew,linksnew,maxconnections,10*lmin);
         end
         %remeshing internal dislocation structures
         [rnnew,linksnew,connectivitynew,linksinconnectnew,fsegnew]=remesh_all(rnnew,linksnew,connectivitynew,linksinconnectnew,fsegnew,lmin,lmax,areamin,areamax,MU,NU,a,Ec,mobility,doremesh,dovirtmesh,vertices,...
             uhat,nc,xnodes,D,mx,mz,w,h,d,TriangleCentroids,TriangleNormals);
     end
-
+    
     rn=[rnnew(:,1:3) rnnew(:,7)];
     vn=rnnew(:,4:6);
     links=linksnew;
     connectivity=connectivitynew;
     linksinconnect=linksinconnectnew;
     fseg=fsegnew;
-
+    
     %store run time information
     %time step
     curstep = curstep + 1;
     %data(curstep,1)=dt;
     simTime = simTime+dt;
-
-%    save restart;
-%     if all(rn(:,4)==67) %no more real segments, stop simulation
-%         disp('Dislocation-free real domain. Only virtual segments remain!');
-%         disp('Computing distorted mesh. Please wait...');
-%         [utilda]=visualise(rn,links,NU,D,mx,my,mz,mel,mno,xnodes,nc,...
-%             dx,dy,dz,w,h,d,vertices,uhat);
-%         return;
-%     end
-%save restart;
-%       if (curstep >= 2004)
-%           fprintf("\n")
-%           save(sprintf('20180925_gpu_debug_%d_%d', n_threads, curstep));
-%       end
-%     if mod(curstep, 1000)==0
-%         close all
-%         plotnodes(rn,links,plim,vertices);
-%         pause
-% % %         save(sprintf('./mat_files/debris_%d', curstep));
-%     end
-%     if -Fend(curstep)*amag^2*mumag < -2580
-%         save(sprintf('./mat_files/20200128_%d_%d_%d_HYmob', curstep, a_trac, simple));
-%         pause
-%     end
+    
+    %    save restart;
+    %     if all(rn(:,4)==67) %no more real segments, stop simulation
+    %         disp('Dislocation-free real domain. Only virtual segments remain!');
+    %         disp('Computing distorted mesh. Please wait...');
+    %         [utilda]=visualise(rn,links,NU,D,mx,my,mz,mel,mno,xnodes,nc,...
+    %             dx,dy,dz,w,h,d,vertices,uhat);
+    %         return;
+    %     end
+    %save restart;
+    %       if (curstep >= 2004)
+    %           fprintf("\n")
+    %           save(sprintf('20180925_gpu_debug_%d_%d', n_threads, curstep));
+    %       end
+    %     if mod(curstep, 1000)==0
+    %         close all
+    %         plotnodes(rn,links,plim,vertices);
+    %         pause
+    % % %         save(sprintf('./mat_files/debris_%d', curstep));
+    %     end
+    %     if -Fend(curstep)*amag^2*mumag < -2580
+    %         save(sprintf('./mat_files/20200128_%d_%d_%d_HYmob', curstep, a_trac, simple));
+    %         pause
+    %     end
     if (mod(curstep, 1000) == 0)
         close all
         save(sprintf('./mat_files/HaiMesha_%d', curstep));
-%         save(sprintf('./mat_files/20200128_%d_%d_%d_HYmob', curstep, a_trac, simple));
-%         save(sprintf('./mat_files/20191216_%d_%d_%d_HYmob', curstep, a_trac, simple));
-%         save(sprintf('./mat_files/20180928_%d', curstep));
-%         save(sprintf('20181001_gpu_%d_%d', n_threads, curstep));
+        %         save(sprintf('./mat_files/20200128_%d_%d_%d_HYmob', curstep, a_trac, simple));
+        %         save(sprintf('./mat_files/20191216_%d_%d_%d_HYmob', curstep, a_trac, simple));
+        %         save(sprintf('./mat_files/20180928_%d', curstep));
+        %         save(sprintf('20181001_gpu_%d_%d', n_threads, curstep));
     end
- end
-
+end
 
 disp('completed')
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+end
