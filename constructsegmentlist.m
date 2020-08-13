3205b389cd2724dbf3c8c6aea49fa7d6b240cb32
Daniel Celis Garza <dcelisgarza@users.noreply.github.com>
2020-08-13 17:17:57 +0100

    Condense if statement.

--- constructsegmentlist.m
+++ constructsegmentlist.m
@@ -9,10 +9,8 @@ for i=1:LINKMAX
     n0=links(i,1);
     n1=links(i,2);
     if((n0~=0)&&(n1~=0))
-        if novirtsegs==1
-            if (rn(n0, end) == 67 || rn(n1, end) == 67 )
-                continue
-            end
+        if novirtsegs == true && (rn(n0, end) == 67 || rn(n1, end) == 67)
+            continue
         end
         nseg=nseg+1;
         segments(nseg,:)=[links(i,1:5),rn(n0,1:3),rn(n1,1:3),links(i,6:8)];
